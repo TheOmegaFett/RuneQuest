8fbf839d2dc96dd02cb83061e7d549c59971d135
tanermaddocks <maddockstaner@gmail.com>
2025-02-22 12:38:43 +1000

    build: merged files from Shane and Taner, ensured no current errors

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv")
 
 /**
  * Establishes connection to MongoDB database for RuneQuest application
@@ -7,17 +8,22 @@ const mongoose = require("mongoose");
  * @returns {Promise<void>}
  * @throws {Error} If connection fails, exits process with status code 1
  */
+
+// Configure dotenv
+dotenv.config()
+
 const connectDB = async () => {
+  const dbURL = process.env.DATABASE_URL || "mongodb://localhost:27017/runequest"
   try {
     // Attempt to connect to local MongoDB instance with specified database name
-    const conn = await mongoose.connect("mongodb://localhost:27017/runequest", {
+    const conn = await mongoose.connect(dbURL, {
       // Configuration options for MongoDB connection
-      useNewUrlParser: true, // Use new URL string parser
-      useUnifiedTopology: true, // Use new Server Discovery and Monitoring engine
+      // useNewUrlParser: true, // Use new URL string parser THIS IS DEPRECATED
+      // useUnifiedTopology: true, // Use new Server Discovery and Monitoring engine THIS IS DEPRECATED
     });
 
     // Log successful connection with host information
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host} => ${dbURL}`);
   } catch (error) {
     // Log any connection errors and terminate process
     console.error(`Error: ${error.message}`);
@@ -25,5 +31,12 @@ const connectDB = async () => {
   }
 };
 
+const disconnectDB = async () => {
+  await mongoose.connection.close()
+}
+
 // Export the connection function for use in other modules
-module.exports = connectDB;
+module.exports = {
+  connectDB,
+  disconnectDB
+}
