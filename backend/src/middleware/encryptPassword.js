cab68303f23691fbfca732e78eb2ca292199f703
tanermaddocks <maddockstaner@gmail.com>
2025-03-12 10:27:15 +1000

    style: made changes to conform to style guide

--- backend/src/middleware/encryptPassword.js
+++ backend/src/middleware/encryptPassword.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose")
 const crypto = require("node:crypto")
 
 exports.encryptPassword = async function (next) {
-    if (!this.isModified("password")) {
-      return next();
-    }
-
-    this.salt = crypto.randomBytes(8).toString("hex");
-  
-    this.password = crypto.scryptSync(this.password, this.salt, 8).toString("hex");
-  
-    next();
+  if (!this.isModified("password")) {
+    return next();
   }
+
+  this.salt = crypto.randomBytes(8).toString("hex");
+
+  this.password = crypto.scryptSync(this.password, this.salt, 8).toString("hex");
+
+  next();
+}
 
