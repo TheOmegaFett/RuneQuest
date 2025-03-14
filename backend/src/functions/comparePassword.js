13c6c5498d05b74098cac7f709616698c64b62a1
tanermaddocks <maddockstaner@gmail.com>
2025-03-14 16:26:55 +1000

    feat: compare password implemented, deleteAll users excludes systemadmin

---
+++ backend/src/functions/comparePassword.js
@@ -0,0 +1,13 @@
+const crypto = require("node:crypto");
+
+exports.comparePassword = function async(
+    existingPassword,
+    existingSalt,
+    providedPassword
+) {
+    return crypto.scryptSync(
+        providedPassword,
+        existingSalt,
+        64
+    ).toString("hex") == existingPassword
+};
--- backend/src/functions/encryptPassword.js
+++ backend/src/functions/encryptPassword.js
@@ -3,9 +3,9 @@ const crypto = require("node:crypto")
 
 exports.encryptPassword = function(password) {
 
-  salt = crypto.randomBytes(12).toString("hex");
+  salt = crypto.randomBytes(64).toString("hex");
 
-  password = crypto.scryptSync(password, salt, 12).toString("hex");
+  password = crypto.scryptSync(password, salt, 64).toString("hex");
 
   return {password, salt}
 };
