548aaf2b0bd53423958ac5f17d6d9c6de9646dcd
tanermaddocks <maddockstaner@gmail.com>
2025-03-15 14:42:34 +1000

    feat: moved all functions in functions directory to helpers and renamed and commented on them

--- backend/src/helpers/filterDataHelper.js
+++ backend/src/helpers/filterDataHelper.js
@@ -1,10 +1,15 @@
-// Function for returning filtered userData to exclude password & salt
+/**
+ * Helper function to filter user data
+ * @param {Object} userData - The user's data
+ * @returns {Object} Filtered user data
+ */
+
 exports.filterUserData = function (userData) {
-    return {
-      id: userData.id,
-      username: userData.username,
-      isAdmin: userData.isAdmin,
-      preferences: userData.preferences,
-      progress: userData.progress,
-    }
-  };
+  return {
+    id: userData.id,
+    username: userData.username,
+    isAdmin: userData.isAdmin,
+    preferences: userData.preferences,
+    progress: userData.progress,
+  }
+};
