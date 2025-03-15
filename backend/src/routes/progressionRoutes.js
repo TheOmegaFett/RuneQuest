b8e2544493b719dff6985793e2d2b2e8345111b5
tanermaddocks <maddockstaner@gmail.com>
2025-03-15 13:50:33 +1000

    chore: change layout to conform to style guide

--- backend/src/routes/progressionRoutes.js
+++ backend/src/routes/progressionRoutes.js
@@ -4,8 +4,16 @@ const progressionController = require("../controllers/progressionController");
 const { checkAuthority } = require("../middleware/checkAuthority");
 
 // All progression routes need authentication - users should only access their own progress
-router.get("/:userId", checkAuthority, progressionController.getUserProgress);
-router.post("/quiz", checkAuthority, progressionController.updateQuizProgress);
+router.get(
+  "/:userId",
+  checkAuthority,
+  progressionController.getUserProgress
+);
+router.post(
+  "/quiz",
+  checkAuthority,
+  progressionController.updateQuizProgress
+);
 router.post(
   "/reading",
   checkAuthority,
