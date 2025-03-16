3afbf4a2e3ce242368da923e7096ad92701fb802
tanermaddocks <maddockstaner@gmail.com>
2025-03-16 13:11:26 +1000

    feat: renamed several file to be consistent and added subfolders to styles folder, began working on landing page and login form

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "../styles/header.css";
+
+// Header component
+
+export function Header() {
+  return (
+    <header>
+      <div className="menu-button">|||</div>
+      <h1>RuneQuest</h1>
+      <div className="profile-link">:D</div>
+    </header>
+  );
+};
---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,20 @@
+import { Header } from "../components/Header";
+
+export function Landing() {
+
+  return (
+    <>
+      <Header />
+
+      <section className="overview">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo unde
+          deleniti iusto laborum provident repellendus! Esse sequi dignissimos
+          temporibus magnam dolorum, asperiores praesentium fugit unde
+          corrupti sint, tempore aut nihil.
+        </p>
+      </section>
+    </>
+  )
+  
+};
