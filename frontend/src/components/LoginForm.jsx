e77796dd99a109bbe058ac06f17ed4973ce58949
tanermaddocks <maddockstaner@gmail.com>
2025-03-17 13:53:47 +1000

    feat: added styling for header component and by extension the landing page

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,15 +1,26 @@
+import "../styles/components/LoginForm.css";
+
 // Dynamic form that allows users to login to or register their account 
 
 export function LoginForm() {
+
   return (
-    <section className="LoginForm">
-      <form>
-        <label htmlFor="username">Username:</label>
-        <input type="text" id="username" name="username" />
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
+    <form className="LoginForm">
+      <section className="input-group">
+        <div className="username-block">
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" />
+        </div>
+        <div className="password-block">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+        </div>
+      </section>
+      <section className="submit-group">
+        <button>Register</button>
         <button>Login</button>
-      </form>
-    </section>
+      </section>
+    </form>
   )
+
 };
