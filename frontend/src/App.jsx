a83515502ef9e489732e8dad4456b0f59c052988
tanermaddocks <maddockstaner@gmail.com>
2025-03-20 16:50:03 +1000

    feat: added extra tab for puzzles, re-enabled landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,30 +17,29 @@ function App() {
   const urlParams = new URLSearchParams(window.location.search);
   const page = urlParams.get("page");
 
-  // // If no userJwt, render landing page
-  // let [userJwt] = useUserJwt();
-  // if (!userJwt.accessToken) {
-  //   return (
-  //     <Landing />
-  //   );
-  // }
-  // // Else render other pages based on URL parameters
-  // else {
-
-  // Conditional rendering based on the page parameter
-  switch (page) {
-    case "profile":
-      return <Profile />;
-    case "learning":
-      return <RuneLearningPage />;
-    case "casting":
-      return <RuneCastingTestPage />;
-    case "puzzles":
-      return <RunePuzzlePage />;
-    case "quiz":
-      return <QuizPage />;
-    default:
-      return <Dashboard />;
+  // If no userJwt, render landing page
+  let [userJwt] = useUserJwt();
+  if (!userJwt.accessToken) {
+    return (
+      <Landing />
+    );
   }
-}
+  else {
+    // Conditional rendering based on the page parameter
+    switch (page) {
+      case "profile":
+        return <Profile />;
+      case "learning":
+        return <RuneLearningPage />;
+      case "casting":
+        return <RuneCastingTestPage />;
+      case "puzzles":
+        return <RunePuzzlePage />;
+      case "quiz":
+        return <QuizPage />;
+      default:
+        return <Dashboard />;
+    }
+  }
+};
 export default App;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,7 +5,7 @@ import "./styles/Dashboard.css"
 // Dashboard page
 export function Dashboard() {
 
-  let [focus, setFocus] = useState(['1', '0', '0'])
+  let [focus, setFocus] = useState(['1', '0', '0', '0'])
 
   return (
     <main className="Dashboard">
@@ -13,16 +13,16 @@ export function Dashboard() {
       <section className="content">
         <button
           className="learning"
-          onClick={() => setFocus(['1', '0', '0'])}
+          onClick={() => setFocus(['1', '0', '0', '0'])}
         >
-          Learning
+          Learn
         </button>
         <article className="learning" style={{ zIndex: focus[0] }}>
           Learning Module
         </article>
         <button
           className="quizzes"
-          onClick={() => setFocus(['0', '1', '0'])}
+          onClick={() => setFocus(['0', '1', '0', '0'])}
         >
           Quizzes
         </button>
@@ -30,16 +30,24 @@ export function Dashboard() {
           Quiz Module
         </article>
         <button
-          className="casting"
-          onClick={() => setFocus(['0', '0', '1'])}
+          className="puzzles"
+          onClick={() => setFocus(['0', '0', '1', '0'])}
         >
-          Casting
+          Puzzles
         </button>
-        <article className="casting" style={{ zIndex: focus[2] }}>
+        <article className="puzzles" style={{ zIndex: focus[2] }}>
+          Puzzle Module
+        </article>
+        <button
+          className="casting"
+          onClick={() => setFocus(['0', '0', '0', '1'])}
+        >
+          Cast
+        </button>
+        <article className="casting" style={{ zIndex: focus[3] }}>
           Casting Module
         </article>
       </section>
     </main>
   )
-
 };
