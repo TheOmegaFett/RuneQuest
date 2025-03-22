8f348af16f44de7c7ccd2544594a1a4a450b1c03
tanermaddocks <maddockstaner@gmail.com>
2025-03-22 21:44:56 +1000

    feat: adjusted styling for dashboard to fix content box sizing issue

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -8,7 +8,7 @@ import { RuneLearningPage } from "./RuneLearningPage";
 
 // Dashboard page
 export function Dashboard() {
-  let [focus, setFocus] = useState(["1", "0", "0", "0"]);
+  let [focus, setFocus] = useState(["flex", "none", "none", "none"]);
 
   return (
     <main className="Dashboard">
@@ -16,41 +16,45 @@ export function Dashboard() {
       <section className="content">
         <button
           className="learning"
-          onClick={() => setFocus(["1", "0", "0", "0"])}
+          id="tab-button"
+          onClick={() => setFocus(["flex", "none", "none", "none"])}
         >
           Learn
         </button>
-        <article className="learning" style={{ zIndex: focus[0] }}>
+        <article className="learning" style={{ display: focus[0] }}>
           {/* Learning Module */}
           <RuneLearningPage />
         </article>
         <button
           className="quizzes"
-          onClick={() => setFocus(["0", "1", "0", "0"])}
+          id="tab-button"
+          onClick={() => setFocus(["none", "flex", "none", "none"])}
         >
           Quizzes
         </button>
-        <article className="quizzes" style={{ zIndex: focus[1] }}>
+        <article className="quizzes" style={{ display: focus[1] }}>
           {/* Quiz Module */}
           <QuizPage />
         </article>
         <button
           className="puzzles"
-          onClick={() => setFocus(["0", "0", "1", "0"])}
+          id="tab-button"
+          onClick={() => setFocus(["none", "none", "flex", "none"])}
         >
           Puzzles
         </button>
-        <article className="puzzles" style={{ zIndex: focus[2] }}>
+        <article className="puzzles" style={{ display: focus[2] }}>
           {/* Puzzle Module */}
           <RunePuzzlePage />
         </article>
         <button
           className="casting"
-          onClick={() => setFocus(["0", "0", "0", "1"])}
+          id="tab-button"
+          onClick={() => setFocus(["none", "none", "none", "flex"])}
         >
           Cast
         </button>
-        <article className="casting" style={{ zIndex: focus[3] }}>
+        <article className="casting" style={{ display: focus[3] }}>
           {/* Casting Module */}
           <RuneCastingPage />
         </article>
